9f4ded3112d7a8d865ab827e9565560f7b6dad47
Jade <hisui.gurin@gmail.com>
2025-05-22 13:04:31 +0200

    Updated ontology with description

--- Scripts/make_entity_script.py
+++ Scripts/make_entity_script.py
@@ -1,22 +1,25 @@
-# This script takes an xlsx file as outputed by extract_script.py after it is annotated
-# with an additional colum for the type of entity and generates a turtle file
+# This script takes entities_annotated.csv as outputed by extract_script.py after it 
+# is annotated with an additional colum for the type of entity and generates 
+# a turtle file
 
 import sys
 import pandas as pd
 
 def make_turtle_entity(nid, name, translation, entity_type):
     entity_uri = f":{name.replace('(', '_').replace(')', '')}"
-    lines = [
-        f"{entity_uri} a :{entity_type} ;",
-        f'    dcterms:identifier "{nid}" ;',
-        f'    rdfs:label "{entity_uri}" ;',
-        f'    :translation "{translation}" .\n'
-    ]
+    lines = []
+    if entity_type == "Object":
+        lines = [
+            f"{entity_uri} a :{entity_type} ;",
+            f'    dcterms:identifier "{nid}" ;',
+            f'    rdfs:label "{entity_uri}" ;',
+            f'    :translation "{translation}" .\n\n'
+        ]
     return "\n".join(lines)
     
 
 def main(output_path):
-    csv_content = open('./Inputs/entities.csv')
+    csv_content = open('./Inputs/entities_annotated.csv')
     df = pd.read_csv(csv_content, header=0)
     with open(output_path, "w", encoding="utf-8") as f:
         f.write("@prefix : <http://example.org/entities#> .\n"
